2e1ba3aaf3606b27ffbdde39bf8e6d46e5c69cc4
PravalVerma <pravalverma2003@gmail.com>
2025-03-05 02:36:26 +0530

    data transformation done-2

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import dill
+import pickle
 
 import numpy as np
 import pandas as pd
@@ -9,15 +9,16 @@ sys.path.append(str(Path(__file__).parent.parent))
 
 from exception import CustomException
 
-def save_objects(file_path,obj):
+def save_object(file_path,obj):
     """
     This function is responsible for saving the objects
     """
     try:
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
+
         with open(file_path, 'wb') as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
